8ebd683d7a81026868c452708c18c1e8c30f8679
carlosfernandezcabrero <carlitostrabajo2018@outlook.es>
2021-04-26 18:09:28 +0000

    Add file property for set database properties

--- src/main/java/com/cfernandez/samplespringbootswaggerapi/SampleSpringbootSwaggerApiApplication.java
+++ src/main/java/com/cfernandez/samplespringbootswaggerapi/SampleSpringbootSwaggerApiApplication.java
@@ -2,8 +2,10 @@ package com.cfernandez.samplespringbootswaggerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:database.properties")
 public class SampleSpringbootSwaggerApiApplication {
 
 	public static void main(String[] args) {
