f45a6ca7c3d551279efe0c1c6c832b095770a760
carlosfernandezcabrero <carlitostrabajo2018@outlook.es>
2021-04-26 15:50:21 +0000

    first commit

---
+++ src/main/java/com/cfernandez/samplespringbootswaggerapi/SampleSpringbootSwaggerApiApplication.java
@@ -0,0 +1,13 @@
+package com.cfernandez.samplespringbootswaggerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringbootSwaggerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringbootSwaggerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cfernandez/samplespringbootswaggerapi/SampleSpringbootSwaggerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfernandez.samplespringbootswaggerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringbootSwaggerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
