6e4834e9b78f240be5cc05092575de55f543f3de
carlosfernandezcabrero <carlitostrabajo2018@outlook.es>
2021-04-29 18:25:21 +0200

    Add test for find soldier by id

--- src/test/java/com/cfernandez/samplespringbootswaggerapi/application/service/SoldierServiceTest.java
+++ src/test/java/com/cfernandez/samplespringbootswaggerapi/application/service/SoldierServiceTest.java
@@ -2,38 +2,39 @@ package com.cfernandez.samplespringbootswaggerapi.application.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import com.cfernandez.samplespringbootswaggerapi.application.out.SoldierPort;
 import com.cfernandez.samplespringbootswaggerapi.model.Soldier;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+@DisplayName("Soldier service")
 class SoldierServiceTest {
 
-    @InjectMocks
-    private SoldierService soldierService;
+    @InjectMocks private SoldierService soldierService;
 
-    @Mock
-    private SoldierPort soldierPort;
+    @Mock private SoldierPort soldierPort;
 
+    private static final Integer ID = 1;
     private static Soldier SOLDIER;
-    private static List<Soldier> SOLDIERS;
 
     @BeforeEach
     void setUp(){
         MockitoAnnotations.openMocks(this);
-        SOLDIERS = new ArrayList<>();
 
         SOLDIER = new Soldier();
-        SOLDIER.setId(1);
+        SOLDIER.setId(ID);
         SOLDIER.setName("My soldier");
         SOLDIER.setCombatPower(90);
         SOLDIER.setStrengthDistanceSoldiers(90);
@@ -43,18 +44,28 @@ class SoldierServiceTest {
         SOLDIER.setFood(50);
         SOLDIER.setType("T");
         SOLDIER.setDescription("this is a test");
-
-        SOLDIERS.add(SOLDIER);
     }
 
     @Test
+    @DisplayName("Find all")
     void testFindAll(){
-        Mockito.when(soldierPort.findAll()).thenReturn(SOLDIERS);
+        Mockito.when(soldierPort.findAll()).thenReturn(Collections.singletonList(SOLDIER));
 
         final List<Soldier> response = soldierService.findAll();
 
         assertNotNull(response);
         assertEquals(1, response.size());
+    }
+    
+    @Test
+    @DisplayName("Find by ID and return data")
+    void testFindById() {
+    	Mockito.when(soldierPort.findById(Mockito.any(Soldier.class))).thenReturn(Optional.of(SOLDIER));
+    	
+    	final Optional<Soldier> response = soldierService.findById(ID);
+    	
+    	assertNotNull(response);
+    	assertTrue(response.isPresent());
     }
     
 }
